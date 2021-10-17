dd2a5c4a079d962108d98acfc8986853c12e2cdd
Lee Naeun <nanni4287@gmail.com>
2021-10-17 22:21:37 +0900

    로그인 회원가입 하기

--- app.ts
+++ app.ts
@@ -3,7 +3,7 @@ import * as readline from 'readline-sync';
 
 function main(): void {
   while (true) {
-    console.log('숫자야구를 시작합니다!');
+    console.log('⚾ 숫자야구를 시작합니다! ⚾');
     console.log(loginOrSignup());
     console.log();
   }
@@ -14,7 +14,7 @@ function loginOrSignup(): string {
   console.log('2. 회원가입');
   const loginInput: string = readline.question();
   if (loginInput === '1') {
-    return '로그인!';
+    return login();
   } else if (loginInput === '2') {
     return signup();
   } else {
@@ -39,8 +39,50 @@ function signup(): string {
   if (Number(nickName)) {
     return '숫자만으로 아이디를 만들 수 없습니다.';
   } else {
-    return '회원가입이 완료되었습니다.';
+    return '🎉 회원가입이 완료되었습니다. 🎉';
   }
 }
+
+function login(): string {
+  console.log();
+  console.log('아이디를 입력해주세요.');
+  const loginInput: string = readline.question();
+  const userParse: any = JSON.parse(fs.readFileSync('./test.txt', 'utf8'));
+
+  try {
+    userParse;
+  } catch {
+    console.log('회원가입 먼저 해주세요.');
+  }
+
+  const filterUser: any = userParse.filter(
+    (el: any) => el.nickName === loginInput
+  );
+
+  if (filterUser.length === 0) {
+    return '🤔 회원정보가 없습니다. 회원가입 먼저 해주세요. 🤔';
+  } else {
+    console.log();
+    console.log(`😍 환영합니다, ${filterUser[0].nickName}님 😍`);
+    console.log();
+    return selectGame();
+  }
+}
+
+function selectGame(): string {
+  console.log('메뉴를 선택해주세요.');
+  console.log('1. 게임시작');
+  console.log('2. 내 최고기록 보기');
+  console.log('3. 종료');
+  const selectMenu = readline.question();
+
+  return 'hi';
+}
+
+function gameStart() {}
+
+function searchBestRecord() {}
+
+function theEnd() {}
 
 main();
